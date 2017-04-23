9128eeea48538b3ee7a25b4395d635fc939956d0
Enrique Rojas Reyes <erojasr@unicartagena.edu.co>
2017-04-23 00:02:16 -0500

    comentario inicial

---
+++ asp_appsistencia/asp_appsistencia/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="asp_appsistencia.MvcApplication" Language="C#" %>
---
+++ asp_appsistencia/asp_appsistencia/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(asp_appsistencia.Startup))]
+namespace asp_appsistencia
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
